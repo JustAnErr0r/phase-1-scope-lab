ccc33f8014b4f90003a7c61bbf65dca4084231f0
Madeleine Heineman <mheineman16@gmail.com>
2022-09-01 15:23:39 -0400

    commit tree e5c8c41886c925611de42bd267c1470d875d2804

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(name){
+    return customerName = 'BOB'
+}
+
+
+function setBestCustomer(who){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(who){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'sally'
+
+function changeLeastFavoriteCustomer(jkasd) {
+    return leastFavoriteCustomer = 'sally'
+}
